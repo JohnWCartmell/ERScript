abaa8a4bfce0a2c118969e6b20f57c31a35751b5
JohnWCartmell <john.w.cartmell@gmail.com>
2022-11-02 08:55:10 +0000

    developing modularisation

--- scripts/assemble.bat
+++ scripts/assemble.bat
@@ -2,11 +2,11 @@
 
 REM run this from the folder which has the src xml file
 
-if not exist ..\proto mkdir ..\proto
+if not exist ..\temp mkdir ..\temp
 
 set filename=%1
 set filenamebase=%filename:~0,-4%
 
-call ..\..\ERmodel_v1.5\scripts\set_path_variables
+call %~dp0\scripts\set_path_variables
 
-java -jar %SAXON_PATH%\saxon9he.jar -s:%filenamebase%.xml -xsl:ERmodelAssemble.xslt -o:output.xml
+java -jar %SAXON_PATH%\saxon9he.jar -s:%filenamebase%.xml -xsl:%ERHOME%\xslt\ERmodel.assemble.xslt -o:..\temp\%filenamebase%.assembled.xml
