96d82c8d87d256c5d7fd8f6fa87867e4c3dd7b49
JohnWCartmell <john.w.cartmell@gmail.com>
2022-11-25 10:30:46 +0000

    rationalise(?) meta model filenames and add build script

---
+++ scripts/buildMetaModel.bat
@@ -0,0 +1,39 @@
+@echo off
+REM run this from the xml folder which has the meta model src files:
+REM See  readme.md for an explanation of the source files.
+
+
+if not exist ..\docs mkdir ..\docs
+
+call %~dp0\set_path_variables
+
+REM java -jar %SAXON_JAR% -s:%filenamebase%.xml -xsl:%ERHOME%\xslt\ERmodel2.svg.xslt -o:..\docs\%filenamebase%.svg filestem=%filenamebase% ERhomeFolderName=%ERHOMEFOLDERNAME%
+
+REM LOGICAL DIAGRAMS AND REPORTING %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+call %ERHOME%\scripts\genSVG ERA..diagram.xml
+
+java -jar %SAXON_JAR% -s:ERA..diagram.xml -xsl:%ERHOME%\xslt\ERmodel2.html.xslt -o:..\docs\ERA..report.html
+
+call %ERHOME%\scripts\genSVG ERAdiagrammed..diagram.xml
+
+java -jar %SAXON_JAR% -s:ERAdiagrammed..diagram.xml -xsl:%ERHOME%\xslt\ERmodel2.html.xslt -o:..\docs\ERAdiagrammed..report.html
+
+REM LOGICAL 2 PHYSICAL %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+java -jar %SAXON_JAR% -s:ERA..logical.xml -xsl:%ERHOME%\xslt\ERmodel2.physical.xslt -o:ERA..physical.xml style=h debug=y
+
+java -jar %SAXON_JAR% -s:ERAdiagrammed..logical.xml -xsl:%ERHOME%\xslt\ERmodel2.physical.xslt -o:ERAdiagrammed..physical.xml style=h debug=y
+
+
+REM PHYSICAL DIAGRAMS AND REPORTING %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+call %ERHOME%\scripts\genSVG ERA..physical..diagram.xml
+
+call %ERHOME%\scripts\genSVG ERAdiagrammed..physical..diagram.xml
+
+call %ERHOME%\scripts\genHtmlReport ERA..physical.xml
+java -jar %SAXON_JAR% -s:ERA..physical..diagram.xml -xsl:%ERHOME%\xslt\ERmodel2.html.xslt -o:..\docs\ERA..physical.report.html
+
+call %ERHOME%\scripts\genHtmlReport ERAdiagrammed..physical.xml
+java -jar %SAXON_JAR% -s:ERAdiagrammed..physical..diagram.xml -xsl:%ERHOME%\xslt\ERmodel2.html.xslt -o:..\docs\ERAdiagrammed..physical.report.html
