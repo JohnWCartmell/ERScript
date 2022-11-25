5ffe83cfadef3caea7ae5dbfab35977e646f1ed5
JohnWCartmell <john.w.cartmell@gmail.com>
2022-11-25 10:32:06 +0000

    debugging

--- scripts/genHierarchical.bat
+++ scripts/genHierarchical.bat
@@ -5,9 +5,10 @@ set filenamebase=%filename:~0,-4%
 
 call %~dp0\set_path_variables
 
-java -jar %SAXON_JAR% -s:%filenamebase%.xml -xsl:%ERHOME%\xslt\ERmodel2.physical.xslt -o:..\temp\%filenamebase%.hierarchical.xml style=h
+java -jar %SAXON_JAR% -s:%filenamebase%.xml -xsl:%ERHOME%\xslt\ERmodel2.physical.xslt -o:..\temp\%filenamebase%.hierarchical.xml style=h debug=y
 
-java -jar %SAXON_JAR% -s:..\temp\%filenamebase%.hierarchical.xml -xsl:%ERHOME%\xslt\ERmodel2.svg.xslt -o:..\docs\%filenamebase%.hierarchical.svg filestem=%filenamebase%.hierarchical
+REM Remove call to genSVG since often will need additional presentation input for a diagram
+REM java -jar %SAXON_JAR% -s:..\temp\%filenamebase%.hierarchical.xml -xsl:%ERHOME%\xslt\ERmodel2.svg.xslt -o:..\docs\%filenamebase%.hierarchical.svg filestem=%filenamebase%.hierarchical 
 
 
 
