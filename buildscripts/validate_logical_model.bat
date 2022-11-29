c8089c49e08b70f78493425faf5ffe0a1444217d
JohnWCartmell <john.w.cartmell@gmail.com>
2022-11-29 21:57:42 +0000

    Delete validate_logical_model.bat

--- buildscripts/validate_logical_model.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-
-rem call %~dp0\set_path_variables
-
-java -jar %JING_PATH%\jing.jar -f %SCHEMAS%\ERmodel.rng %SRCXML%\ERmodelERmodel.core.xml
-
-java -jar %SAXON_JAR% -s:%SRCXML%\ERmodelERmodel.core.xml -xsl:%XSLT%/ERmodelERmodel.referential_integrity.xslt -o:%TEMP%\xml\ERmodelERmodel.validation.out.xml
-
