d5cdf7b33daf4632fdeffac46e946987e10c7610
JohnWCartmell <john.w.cartmell@gmail.com>
2022-12-24 09:00:27 +0000

    Change so that an inforbox is dragged by its header and en passant infoboxBody is resizeable.

--- js/draggable.js
+++ js/draggable.js
@@ -11,15 +11,12 @@ if (document.readyState === 'loading') {  // Loading hasn't finished yet
   initialise();
 }
 
-
-
 function initialise() {
   console.info('initialising');
   svgContainerElement = document.querySelector("#svgcontainer");
   console.log("svg container" + svgContainerElement) ;
-  /*svgContainerElement.style.pointerEvents = "none";*/
 
-  const freeStandingInfoBlocks = document.querySelectorAll(".infobox");
+  const freeStandingInfoBlocks = document.querySelectorAll(".infoboxHeader");
   console.log("topblocls:" + freeStandingInfoBlocks.length )
   for (let i = 0; i < freeStandingInfoBlocks.length; i++) {
     freeStandingInfoBlocks[i].onmousedown=dragMouseDown;
@@ -28,24 +25,22 @@ function initialise() {
 
 /*Globals */
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  var elmnt ="" ;
+  var infoboxElmnt ="" ;
 
   function dragMouseDown(e) {
     e = e || window.event;
     e.preventDefault();  /* why? */
-    elmnt = e.currentTarget;
-    console.log("start drag myclass " + elmnt.className + "id" + elmnt.id)
-    console.log("visibility",elmnt.style.visibility)
-    for (const child of elmnt.children) {
-        console.log(' child id', child.id);
-    }
+    infoboxElmnt = e.currentTarget.parentNode;
+    console.log("start drag myclass '" + infoboxElmnt.className + "'' id: " + infoboxElmnt.id)
+    console.log("visibility",infoboxElmnt.style.visibility)
+
     // get the mouse cursor position at startup:
     pos3 = e.clientX;
     pos4 = e.clientY;
     document.onmouseup = closeDragElement;
     // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
-    console.log("continue drag myclass " + elmnt.className + "id" + elmnt.id)
+    console.log("continue drag myclass " + infoboxElmnt.className + "id" + infoboxElmnt.id)
     svgContainerElement.style.pointerEvents = "none";
   }
 
@@ -54,18 +49,16 @@ function initialise() {
     e = e || window.event;
     e.preventDefault();
     // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
+    pos1 = pos3 - e.clientX
     pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
     pos4 = e.clientY;
     // set the element's new position: avoid going negative
-    elmnt.style.top = Math.max(elmnt.offsetTop - pos2,0) + "px";  
-    elmnt.style.left = Math.max(elmnt.offsetLeft - pos1,0) + "px";
-    //console.log(" dragging myclass " + elmnt.className + "id" + elmnt.id)
+    infoboxElmnt.style.top = Math.max(infoboxElmnt.offsetTop - pos2,0) + "px";  
+    infoboxElmnt.style.left = Math.max(infoboxElmnt.offsetLeft - pos1,0) + "px";
   }
 
   function closeDragElement() {
-     //console.log("close drag myclass " + elmnt.className + "id" + elmnt.id)
     // stop moving when mouse button is released:
     document.onmouseup = null;
     document.onmousemove = null;
