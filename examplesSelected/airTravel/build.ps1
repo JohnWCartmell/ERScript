147eacdaa6911e71e6974dedc8c31e64dc408d6b
JohnWCartmell <john.w.cartmell@gmail.com>
2025-03-03 09:07:51 +0000

    Move at least some of the airTravel example into a selected example.

---
+++ examplesSelected/airTravel/build.ps1
@@ -0,0 +1,52 @@
+$commandFolder=Split-Path $MyInvocation.MyCommand.Path
+
+. ($commandFolder + '\..\..\buildscripts\setBuildtimePathVariables.ps1')
+
+echo ("*** building from  $SOURCE airTravel folder")
+
+$SOURCEXML = $SOURCE + '\examplesSelected\airTravel'
+$TARGETXML = $TARGET + '\examplesSelected\airTravel\xml'
+$TARGETSVGFOLDER = $TARGET + '\www.entitymodelling.org\svg'
+$TARGETTEXFOLDER = $TARGET + '\docs\images'
+
+# CREATE target folder if it doesn't already exist
+If(!(test-path -PathType container $TARGETXML))
+{
+      echo ('CREATING target folder ' + $TARGETXML)
+      New-Item -ItemType Directory -Path $TARGETXML
+}
+
+# COPY xml files
+attrib -R $TARGETXML\*.xml
+copy-item -Path $SOURCEXML\*.xml -Destination $TARGETXML
+attrib +R $TARGETXML\*.xml
+attrib -R $TARGETXML\*..physical.xml    #these are generated and therefore need to be overwriteable
+
+pushd $TARGETXML
+if ($false)
+{
+echo 'airTravel Example'
+echo 'Run surface ER.instanceValidation.xslt on logical model'
+. $TARGET\scripts\ER.instanceValidation.ps1 airTravel..logical.xml -outputFolder ..\docs
+}
+      
+echo 'airTravel Example'
+. $TARGET\scripts\buildExampleSVG.ps1 airTravel       `
+                             -svgOutputFolder $TARGETSVGFOLDER `
+                             -texOutputFolder $TARGETTEXFOLDER `
+                             -animate -shortSeparator NA -longSeparator NA      
+if ($false)
+{
+echo 'airTravel Barker Figure 3-18 '
+. $TARGET\scripts\buildExampleSVG.ps1 airTravelBarkerFigure3-18       `
+                             -svgOutputFolder $TARGETSVGFOLDER `
+                             -texOutputFolder $TARGETTEXFOLDER `
+                             -animate -shortSeparator NA -longSeparator NA
+echo 'airTravel Flex version'
+. $TARGET\flexDiagramming\scripts\er2flex2svg.ps1 airTravel..logical.xml -animate
+}
+
+popd 
+
+
+
