b68c5e5680b3d97b184f7be8859a3eec3f9101ce
JohnWCartmell <john.w.cartmell@gmail.com>
2025-03-18 17:47:00 +0000

    New diagrams for the dramatic arts and change the build to be generic. Builds all ..diagram.xml examples.

--- examplesSelected/theDramaticArts/build.ps1
+++ examplesSelected/theDramaticArts/build.ps1
@@ -1,135 +1,79 @@
+
+Param(
+   [Parameter(Mandatory=$False)]
+       [string]$filename
+)
+
 $commandFolder=Split-Path $MyInvocation.MyCommand.Path
 
 . ($commandFolder + '\..\..\buildscripts\setBuildtimePathVariables.ps1')
 
-echo ("*** building from  $SOURCE theDramaticArts folder")
+$EXAMPLEFOLDERNAME='examplesSelected\theDramaticArts'
 
-$SOURCEXML = $SOURCE + '\examplesSelected\theDramaticArts'
-$TARGETXML = $TARGET + '\examplesSelected\theDramaticArts\xml'
-$TARGETSVGFOLDER = $TARGET + '\www.entitymodelling.org\svg'
+$SOURCEXML = $SOURCE  +  '\' + $EXAMPLEFOLDERNAME
+$TARGETXML = $TARGET + '\' + $EXAMPLEFOLDERNAME +'\xml'
+$TARGETWWW = $TARGET + '\www.entitymodelling.org'
+$TARGETSVGFOLDER = $TARGETWWW + '\svg'
+# $TARGETDOCS = $TARGET + '\' + $EXAMPLEFOLDERNAME + '\docs'
 $TARGETTEXFOLDER = $TARGET + '\docs\images'
 
-# CREATE target folder if it doesn't already exist
+$file=Get-Item $filename
+echo ('basename is:' + $file.Basename) 
+echo ('extension is:' + $file.Extension)
+
+if ($PSBoundParameters.ContainsKey('filename') -and ($filename -notmatch '\.\.diagram\.xml$')) {
+    throw "Error: Filename must end with '..diagram.xml'. Given: $filename"
+}
+
+
+# CREATE target xml folder if it doesn't already exist
 If(!(test-path -PathType container $TARGETXML))
 {
       echo ('CREATING target folder ' + $TARGETXML)
       New-Item -ItemType Directory -Path $TARGETXML
 }
 
-# COPY xml files
+# CREATE target docs folder if it doesn't already exist
+#If(!(test-path -PathType container $TARGETDOCS))
+#{
+ #     echo ('CREATING target folder ' + $TARGETDOCS)
+ #     New-Item -ItemType Directory -Path $TARGETDOCS
+#}
+
+# COPY files
 attrib -R $TARGETXML\*.xml
+#copy all xml files # bit ugly but I don't know which ones needed
 copy-item -Path $SOURCEXML\*.xml -Destination $TARGETXML
-attrib +R $TARGETXML\*.xml
-attrib -R $TARGETXML\*..physical.xml    #these are generated and therefore need to be overwriteable
 
-pushd $TARGETXML
 
-if ($false)
-{
-echo 'theDramaticArts Example'
-. $TARGET\scripts\buildExampleSVG.ps1 dramaticArts1       `
+# This function called from temp build folder 
+function Build-File {
+    param (
+        $FileName
+    )
+    $diagramName = $Filename -replace '\.\.diagram\.xml$', ''
+   Write-Output $prefix
+    echo('Building ' +  $diagramName)
+   . $TARGET\scripts\buildExampleSVG.ps1 $diagramName     `
                              -svgOutputFolder $TARGETSVGFOLDER `
                              -texOutputFolder $TARGETTEXFOLDER `
                              -animate -shortSeparator NA -longSeparator NA
-
-echo 'theDramaticArts Annotated '
-. $TARGET\scripts\buildExampleSVG.ps1 dramaticArts1.annotate       `
-                             -svgOutputFolder $TARGETSVGFOLDER `
-                             -texOutputFolder $TARGETTEXFOLDER `
-                             -animate -shortSeparator NA -longSeparator NA
-
-echo 'theDramaticArts Play Playwright Relationship'
-. $TARGET\scripts\buildExampleSVG.ps1 dramaticArtsPlayPlaywright    `
-                             -svgOutputFolder $TARGETSVGFOLDER `
-                             -texOutputFolder $TARGETTEXFOLDER `
-                             -animate -shortSeparator NA -longSeparator NA
-
-echo 'theDramaticArts Character Fragment'
-. $TARGET\scripts\buildExampleSVG.ps1 dramaticArtsCharacterFragment       `
-                             -svgOutputFolder $TARGETSVGFOLDER `
-                             -texOutputFolder $TARGETTEXFOLDER `
-                             -animate -shortSeparator NA -longSeparator NA
-
-echo 'theDramaticArts Dramatic Role Fragment'
-. $TARGET\scripts\buildExampleSVG.ps1 dramaticArtsDramaticRoleFragment       `
-                             -svgOutputFolder $TARGETSVGFOLDER `
-                             -texOutputFolder $TARGETTEXFOLDER `
-                             -animate -shortSeparator NA -longSeparator NA
-
-echo 'theDramaticArts Production Fragment'
-. $TARGET\scripts\buildExampleSVG.ps1 dramaticArtsProductionFragment      `
-                             -svgOutputFolder $TARGETSVGFOLDER `
-                             -texOutputFolder $TARGETTEXFOLDER `
-                             -animate -shortSeparator NA -longSeparator NA      
-
-echo 'theDramaticArts dramaticArtsPortrayalScopeFragment'
-. $TARGET\scripts\buildExampleSVG.ps1 dramaticArtsPortrayalScopeFragment     `
-                             -svgOutputFolder $TARGETSVGFOLDER `
-                             -texOutputFolder $TARGETTEXFOLDER `
-                             -animate -shortSeparator NA -longSeparator NA    
-                  
-echo 'theDramaticArts dramaticArtsPortrayalScope'
-. $TARGET\scripts\buildExampleSVG.ps1 dramaticArtsPortrayalScope     `
-                             -svgOutputFolder $TARGETSVGFOLDER `
-                             -texOutputFolder $TARGETTEXFOLDER `
-                             -animate -shortSeparator NA -longSeparator NA   
-
-echo 'theDramaticArts dramaticArtsPath1'
-. $TARGET\scripts\buildExampleSVG.ps1 dramaticArtsPath1     `
-                             -svgOutputFolder $TARGETSVGFOLDER `
-                             -texOutputFolder $TARGETTEXFOLDER `
-                             -animate -shortSeparator NA -longSeparator NA 
-   
-echo 'theDramaticArts dramaticArtsPath2'
-. $TARGET\scripts\buildExampleSVG.ps1 dramaticArtsPath2     `
-                             -svgOutputFolder $TARGETSVGFOLDER `
-                             -texOutputFolder $TARGETTEXFOLDER `
-                             -animate -shortSeparator NA -longSeparator NA 
-
-echo 'theDramaticArts Character..identificationScheme'
-. $TARGET\scripts\buildExampleSVG.ps1 dramaticArtsCharacter..identificationScheme    `
-                             -svgOutputFolder $TARGETSVGFOLDER `
-                             -texOutputFolder $TARGETTEXFOLDER `
-                             -animate -shortSeparator NA -longSeparator NA   
-
-
-echo 'theDramaticArts Character..linearIdentificationScheme'
-. $TARGET\scripts\buildExampleSVG.ps1 dramaticArtsCharacter..linearIdentificationScheme    `
-                             -svgOutputFolder $TARGETSVGFOLDER `
-                             -texOutputFolder $TARGETTEXFOLDER `
-                             -animate -shortSeparator NA -longSeparator NA
-
-echo 'theDramaticArts production..identificationScheme'
-. $TARGET\scripts\buildExampleSVG.ps1 dramaticArtsProduction..identificationScheme    `
-                             -svgOutputFolder $TARGETSVGFOLDER `
-                             -texOutputFolder $TARGETTEXFOLDER `
-                             -animate -shortSeparator NA -longSeparator NA 
-   
-
-echo 'theDramaticArts production..linearIdentificationScheme'
-. $TARGET\scripts\buildExampleSVG.ps1 dramaticArtsProduction..linearIdentificationScheme    `
-                             -svgOutputFolder $TARGETSVGFOLDER `
-                             -texOutputFolder $TARGETTEXFOLDER `
-                             -animate -shortSeparator NA -longSeparator NA 
-                             
-echo 'theDramaticArts role..identificationScheme'
-. $TARGET\scripts\buildExampleSVG.ps1 dramaticArtsRole..identificationScheme    `
-                             -svgOutputFolder $TARGETSVGFOLDER `
-                             -texOutputFolder $TARGETTEXFOLDER `
-                             -animate -shortSeparator NA -longSeparator NA  
-
-echo 'theDramaticArts role..linearIdentificationScheme'
-. $TARGET\scripts\buildExampleSVG.ps1 dramaticArtsRole..linearIdentificationScheme    `
-                             -svgOutputFolder $TARGETSVGFOLDER `
-                             -texOutputFolder $TARGETTEXFOLDER `
-                             -animate -shortSeparator NA -longSeparator NA 
 }
 
-echo 'theDramaticArts playedBy Relationship'
-. $TARGET\scripts\buildExampleSVG.ps1 dramaticArtsPlayedByRelationship    `
-                             -svgOutputFolder $TARGETSVGFOLDER `
-                             -texOutputFolder $TARGETTEXFOLDER `
-                             -animate -shortSeparator NA -longSeparator NA 
+
+pushd $TARGETXML
+echo ('*********** location: ' + (Get-Location) )
+if ($PSBoundParameters.ContainsKey('filename')){
+    echo ('need build from' + $filename)
+    Build-File -FileName $filename
+}else{
+    echo 'building all ..diagram files'
+    get-ChildItem -Path *..diagram.xml  | Foreach-Object {
+      echo 'building ' + $_.Name
+      Build-File -FileName $_.Name
+    }
+    echo 'done building all'
+}
 
 if ($false)
 {
@@ -138,6 +82,5 @@ echo 'theDramaticArts Flex version'
 }
 
 popd 
-
 
 
